51353e7f83b78b408747f336a99aa8e0a109d6b4
질문KoSeungyu < koeul123@gmail.com>
2022-06-11 21:49:51 +0900

    users

---
+++ src/main/java/com/hanghae/auction/dto/SignupRequestDto.java
@@ -0,0 +1,18 @@
+package com.hanghae.auction.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SignupRequestDto {
+    private String username;
+    private String password;
+    private String passwordcheck;
+
+    public SignupRequestDto(String username, String password, String passwordcheck) {
+        this.username = username;
+        this.password = password;
+        this.passwordcheck = passwordcheck;
+    }
+}
