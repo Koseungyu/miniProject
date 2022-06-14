3ef72095053940dca5e396922900d387b9ae2bb6
질문KoSeungyu < koeul123@gmail.com>
2022-06-14 12:31:22 +0900

    수정 사항

--- src/main/java/com/hanghae/auction/config/WebMvcConfig.java
+++ src/main/java/com/hanghae/auction/config/WebMvcConfig.java
@@ -1,9 +1,9 @@
 //package com.hanghae.auction.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@RequiredArgsConstructor
 //@Configuration
